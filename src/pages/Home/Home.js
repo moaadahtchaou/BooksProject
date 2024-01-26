ed0ace9d0ca9d8fec3fc9de312bbecd93ccd5f3d
ahtchaou moaad <moaadahtchao@gmail.com>
2024-01-26 21:39:41 +0100

    first co

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 const Home = () => {
-    console.log(process.env.REACT_APP_API_KEY )
     return ( 
         <div>Hello, the home page is not finished yet. You can try searching for books using Searchbar</div>
      );
